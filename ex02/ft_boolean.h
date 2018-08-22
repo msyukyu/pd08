d9135b6b4fcf712d95cc28feb1747e68f80ed691
msyukyu <damien_ab@hotmail.com>
2018-08-22 08:05:54 +0200

    ft_boolean

--- ex02/ft_boolean.h
+++ ex02/ft_boolean.h
@@ -13,13 +13,11 @@
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
 # define EVEN_MSG "I have an even number of arguments.\n"
-# define ODD_MSG "I have an odd number of argumets.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 # define TRUE 1
 # define FALSE 0
-# define 
+# define EVEN(X) (X % 2 == 0)
 # include <unistd.h>
 
 typedef int	t_bool;
-void	ft_putstr(char *str);
-t_bool	ft_is_even(int nbr);
 #endif
